5f41853f4bf2753e64b041a49d87cbc707438285
scqsryws <51110732+scqsryws@users.noreply.github.com>
2019-06-03 20:48:11 +0800

    Create drawstar.py

---
+++ blg/drawstar.py
@@ -0,0 +1,14 @@
+# from turtle import Turtle
+import turtle
+# p = Turtle()
+'''
+turtle.speed(1)
+turtle.pensize(3)
+turtle.color("red","yellow")
+
+turtle.begin_fill()
+for i in range(5):
+    turtle.forward(-200)
+    turtle.left(144)
+turtle.end_fill()
+'''
